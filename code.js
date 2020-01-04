3e242cef12722032c3e8f2ef80da3605a7e11f6b
Stefanus J. A <57819092+Stefanus1235@users.noreply.github.com>
2020-01-04 19:20:37 +0700

    Tambahan comment

--- code.js
+++ code.js
@@ -4,35 +4,35 @@ function Add() {
     if (emptyornot == "") return 
 
     else {
-    let todotext = document.createElement("input")
+    let todotext = document.createElement("input") <!--> Bikin elemen input dengan value yang diisi pengguna. -->
     todotext.setAttribute("type", "text")
     todotext.setAttribute("value", inputVal.value)
     todotext.classList.add("todotext")
     todotext.disabled = true
     
-    let tododel = document.createElement("button")
+    let tododel = document.createElement("button") <!-- Bikin elemen button Delete -->
     tododel.classList.add("deleteButton")
     tododel.innerHTML = "Delete"
     tododel.addEventListener("click", function(){
         container.removeChild(todonotes)
     })
     
-    let tanggal = document.createElement("input")
+    let tanggal = document.createElement("input") <!-- Bikin elemen tanggal, dengan value sesuai input pengguna. -->
     tanggal.setAttribute("type", "date")
     tanggal.setAttribute("value", Tggl.value)
     tanggal.classList.add("Date")
     tanggal.disabled = true
 
-    let container = document.querySelector(".todolist")
+    let container = document.querySelector(".todolist") <!-- set variabel container, yang merupakan div dengan kelas todolist -->
 
-    let todonotes = document.createElement("div")
+    let todonotes = document.createElement("div") <!-- set elemen div yang akan ditambahkan kedalam container -->
     
-    container.appendChild(todonotes)
+    container.appendChild(todonotes) <!-- Tambahkan div todonotes ke container, dengan todonotes berisi input text, tanggal, dan button -->
     todonotes.appendChild(todotext)
     todonotes.appendChild(tanggal)
     todonotes.appendChild(tododel)
-    Tggl.value = ""
-    inputVal.value = ""
+    Tggl.value = "" <!-- reset input Tanggal -->
+    inputVal.value = "" <!-- reset input text todo -->
     }
 
 
